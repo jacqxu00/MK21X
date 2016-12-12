e695dafe72a9eb560f7e998e9f4053903a79a44f
Jacqueline Xu <jackiestuy@gmail.com>
2016-12-11 21:22:03 -0500

    donegit add 03Barcode/

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -1,3 +1,6 @@
+import java.lang.*;
+import java.util.*;
+
 public class Barcode implements Comparable<Barcode>{
 // instance variables
    private String _zip;
@@ -8,13 +11,16 @@ public class Barcode implements Comparable<Barcode>{
 //               or zip contains a non digit
 //               _zip and _checkDigit are initialized.
   public Barcode(String zip) {
-      boolean ans = true;
       if (zip.length() == 5) {
+	  boolean ans = true;
 	  for (int i = 0; i < 5; i++) {
-	      ans = ans && Character.isDigit(_zip.charAt(i));
+	      ans = ans && Character.isDigit(zip.charAt(i));
 	  }
 	  if (ans) {
 	  _zip = zip;
+	  }
+	  else {
+	      throw new IllegalArgumentException("zip code contains at least one nondigit");
 	  }
       }	  
       else {
@@ -31,56 +37,97 @@ public class Barcode implements Comparable<Barcode>{
       return sum % 10;
   }
 
-  private static String intToCode(int num) {
-      String[] codes = new String[10];
-      codes = 1  {"||:::",
-		  ":::||",
-		  "::|:|",
-		  "::||:",
-		  ":|::|",
-		  ":|:|:",
-		  ":||::",
-		  "|:::|",
-		  "|::|:",
-		  "|:|::"};
-      return codes[num];
+  private static String intToCode(String num) {
+      ArrayList<String> codes = new ArrayList<String>();
+      codes.add("||:::");
+      codes.add(":::||");
+      codes.add("::|:|");
+      codes.add("::||:");
+      codes.add(":|::|");
+      codes.add(":|:|:");
+      codes.add(":||::");
+      codes.add("|:::|");
+      codes.add("|::|:");
+      codes.add("|:|::");
+      return codes.get(Integer.parseInt(num));
   }
+
+    private static String codeToInt(String code) {
+      ArrayList<String> codes = new ArrayList<String>();
+      codes.add("||:::");
+      codes.add(":::||");
+      codes.add("::|:|");
+      codes.add("::||:");
+      codes.add(":|::|");
+      codes.add(":|:|:");
+      codes.add(":||::");
+      codes.add("|:::|");
+      codes.add("|::|:");
+      codes.add("|:|::");
+      return ""+ codes.indexOf(code);
+    }
+    
 
 //postcondition: format zip + check digit + Barcode 
 //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
   public String toString(){
-      String ans = _zip + _checkDigit + " ";
-      for (int i = 0; i < 5; i++) {
-	  String nextNum = _zip.substring(i,i+1);
-	  ans += inttoCode(Integer.parseInt(nextNum));
-	  }
-      }
+      String ans = _zip + checkSum(_zip) + " " + toCode(_zip);
       return ans;
   }
 
-
-// postcondition: compares the zip + checkdigit, in numerical order. 
+ 
   public int compareTo(Barcode other){
       return this.toString().compareTo(other.toString());
   }
 
-  public static toCode(String zip) {
+  public static String toCode(String zip) {
+      String ans = "|";
       if (zip.length() != 5) {
 	  throw new IllegalArgumentException("zip code must be 5 digits long");
       }
-      else try {
-	  String ans = "";
-	  for (int i = 0; i < 5; i++) {
-	      String nextNum = _zip.substring(i,i+1);
-	  }
-	  return ans;
+      boolean value = true;
+      for (int i = 0; i < 5; i++) {
+	  value = value && Character.isDigit(zip.charAt(i));
       }
-      catch (NumberFormatException e) {
-	  throw e("invalid zip");
+      if (!value) {
+	  throw new NumberFormatException("zip code contains at least one nondigit");
+      }
+      else {
+	  for (int i = 0; i < 5; i++) {
+	      String nextNum = zip.substring(i,i+1);
+	      ans += intToCode(nextNum);
+	  }
+	  ans += intToCode(""+checkSum(zip))+"|";
+	  return ans;
       }
   }
 
-  public static toZip(String code) {
-
+  public static String toZip(String code) {
+      String ans = "";
+      if (code.length() != 32) {
+	  throw new IllegalArgumentException("incorrect length of barcode");
+      }
+      if (code.charAt(0)!= '|' || code.charAt(31)!= '|') {
+	  throw new IllegalArgumentException("incorrect guardrails");
+      }
+      boolean value = true;
+      for (int i = 0; i < 32; i++) {
+	  value = value && (code.charAt(i)=='|' || code.charAt(i)==':');
+      }
+      if (!value) {
+	  throw new IllegalArgumentException("non-barcode characters are used");
+      }
+      boolean result = false;
+      for (int i = 0; i < 6; i++) {
+	  result = result || (codeToInt(code.substring(i*5+1,(i+1)*5+1)).equals("-1"));
+	  ans += codeToInt(code.substring(i*5+1,(i+1)*5+1));
+      }
+      if (result) {
+	  throw new IllegalArgumentException("encoded ints are invalid");
+      }
+      if (!(""+ans.charAt(5)).equals(""+checkSum(ans.substring(0,5)))) {
+        throw new IllegalArgumentException("checksum is incorrect");
+      }
+      else return ans.substring(0,5);
   }
 }
