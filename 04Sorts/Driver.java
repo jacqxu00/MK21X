9bee506d23c4fc16795f2f306fad9717d77f367c
Jacqueline Xu <jackiestuy@gmail.com>
2016-12-13 20:57:49 -0500

    finished testing

--- 04Sorts/Driver.java
+++ 04Sorts/Driver.java
@@ -3,13 +3,14 @@ public class Driver {
 	int[] name = new int[8];
 	name[0] = 5;
 	name[1] = 7;
-	name[2] = 8;
+	name[2] = 7;
 	name[3] = 3;
 	name[4] = 4;
-	name[5] = 2;
+	name[5] = 3;
 	name[6] = 0;
 	name[7] = 1;
+	System.out.println(Sorts.toString(name));
 	Sorts.selectionSort(name);
-	System.out.println(name);
+	System.out.println(Sorts.toString(name));
     }
 }
--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -1,28 +1,42 @@
 public class Sorts{
 
-  public static String name(){
-    return "10.Xu.Jackie"; 
-  }
+    public static String name(){
+	return "10.Xu.Jackie"; 
+    }
 
-  /**Selection sort of an int array. 
-  *Upon completion, the elements of the array will be in increasing order.
-  *@param data the elements to be sorted.
-  */
-  public static void selectionSort(int[] data){
-      for (int i = 0; i < data.length - 1; i++) {
-	  int smallest = data[i];
-	  int j = i;
-	  while (j < data.length - 1) {
-	      if (data[j] < smallest) {
-		  smallest = data[j];
-	      }
-	      j++;
-	  }
-	  if (smallest != data[i]) {
-	      data[j] = data[i];
-	      data[i] = smallest;
-	  }
-      }
-  }
+    /**Selection sort of an int array. 
+     *Upon completion, the elements of the array will be in increasing order.
+     *@param data the elements to be sorted.
+     */
+    public static void selectionSort(int[] data){
+	for (int i = 0; i < data.length - 1; i++) {
+	    int smallest = data[i];
+	    int smallind = i;
+	    int j = i+1;
+	    while (j < data.length) {
+		if (data[j] < smallest) {
+		    smallest = data[j];
+		    smallind = j;
+		}
+		j++;
+	    }
+	   
+	    if (smallest != data[i]) {
+		data[smallind] = data[i];
+		data[i] = smallest;
+	    }
+	    System.out.println(toString(data));
+	}
+    }
 
+    public static String toString(int[] data) {
+	String ans = "[ ";
+	for (int i = 0; i < data.length - 1; i++) {
+	    ans += data[i];
+	    ans += ", ";
+	}
+	ans += data[data.length-1];
+	ans += "]";
+	return ans;
+    }
 }
