a89f1590ccc624cbee81646e17341d2b8c86b6a7
Jacqueline Xu <jackiestuy@gmail.com>
2016-12-14 06:39:03 -0500

    length 0-2 cases

--- 04Sorts/Driver.java
+++ 04Sorts/Driver.java
@@ -1,16 +1,25 @@
 public class Driver {
     public static void main(String[] args) {
-	int[] name = new int[8];
-	name[0] = 5;
-	name[1] = 7;
-	name[2] = 7;
-	name[3] = 3;
-	name[4] = 4;
-	name[5] = 3;
-	name[6] = 0;
-	name[7] = 1;
-	System.out.println(Sorts.toString(name));
-	Sorts.selectionSort(name);
-	System.out.println(Sorts.toString(name));
+	int[] s8 = new int[8];
+	s8[0] = 5;
+	s8[1] = 8;
+	s8[2] = 7;
+	s8[3] = 3;
+	s8[4] = 4;
+	s8[5] = 2;
+	s8[6] = 0;
+	s8[7] = 1;
+	System.out.println(Sorts.toString(s8));
+	Sorts.insertionSort(s8);
+	System.out.println(Sorts.toString(s8));
+	int[] s0 = new int[0];
+	System.out.println(Sorts.toString(s0));
+	Sorts.insertionSort(s0);
+	System.out.println(Sorts.toString(s0));
+	int[] s1 = new int[1];
+	s1[0] = 3;
+	System.out.println(Sorts.toString(s1));
+	Sorts.insertionSort(s1);
+	System.out.println(Sorts.toString(s1));
     }
 }
--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -9,34 +9,61 @@ public class Sorts{
      *@param data the elements to be sorted.
      */
     public static void selectionSort(int[] data){
-	for (int i = 0; i < data.length - 1; i++) {
-	    int smallest = data[i];
-	    int smallind = i;
-	    int j = i+1;
-	    while (j < data.length) {
-		if (data[j] < smallest) {
-		    smallest = data[j];
-		    smallind = j;
+	if (data.length > 1) {
+	    for (int i = 0; i < data.length - 1; i++) {
+		int smallest = data[i];
+		int smallind = i;
+		int j = i+1;
+		while (j < data.length) {
+		    if (data[j] < smallest) {
+			smallest = data[j];
+			smallind = j;
+		    }
+		    j++;
 		}
-		j++;
-	    }
 	   
-	    if (smallest != data[i]) {
-		data[smallind] = data[i];
-		data[i] = smallest;
+		if (smallest != data[i]) {
+		    data[smallind] = data[i];
+		    data[i] = smallest;
+		}
 	    }
-	    System.out.println(toString(data));
 	}
     }
 
-    public static String toString(int[] data) {
+    public static void insertionSort(int[] data){
+	if (data.length > 1) {
+	    for (int i = 1; i < data.length; i++) {
+		if (data[i] < data[i-1]){
+		    int val = data[i];
+		    for (int j = i; j >= 1; j--) {
+			if (val < data[j-1]) {
+			    data[j] = data[j-1];
+			    if (j == 1 || val > data[j-2]) {
+				data[j-1] = val;
+			    }
+			}
+		    }
+		}
+	    }
+	}		
+    }
+
+    public static String toString(int[] data){
 	String ans = "[ ";
-	for (int i = 0; i < data.length - 1; i++) {
-	    ans += data[i];
-	    ans += ", ";
+	if (data.length == 1) {
+	    ans += data[0] + "]";
 	}
-	ans += data[data.length-1];
-	ans += "]";
+	if (data.length == 0) {
+	    ans += "]";
+	}
+	else {
+	    for (int i = 0; i < data.length - 1; i++) {
+		ans += data[i];
+		ans += ", ";
+	    }
+	    ans += data[data.length-1];
+	    ans += "]";
+	}
 	return ans;
     }
 }
